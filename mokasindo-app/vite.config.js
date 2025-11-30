945ae1f49a2b16a2efa79024f558513310d0ad9e
Iqlima Noura Azkiya <23081010220@student.upnjatim.ac.id>
2025-11-30 16:12:22 +0700

    Add additional JS files to Vite config input

--- mokasindo-app/vite.config.js
+++ mokasindo-app/vite.config.js
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js',
+                    'resources/js/owner-overview.js','resources/js/owner-reports-utils.js', 
+                    'resources/js/owner-reports-chartstable.js'],
             refresh: true,
         }),
         tailwindcss(),
